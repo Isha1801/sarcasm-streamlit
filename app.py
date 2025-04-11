33c9933784c9be86ea3d1ca85ef312e51e8375ef
Isha1801 <134993607+Isha1801@users.noreply.github.com>
2025-04-11 18:59:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import re
 
 MODEL_DIR = "model"
 MODEL_PATH = os.path.join(MODEL_DIR, "tf_model.h5")
-SHARE_URL = "https://www.dropbox.com/scl/fi/ik0awndc3jz26ia0mp5v4/tf_model.h5?rlkey=agnmnsxrcs124z3sbf2y4wsez&st=saskrey0&dl=1"
+SHARE_URL = "hhttps://www.dropbox.com/scl/fi/7s8cepv30f5qgtb77itoc/model.zip?rlkey=p2mb6v8jn54ll1l2pa2tle8yu&st=lp42aq6q&dl=1"
 
 # Download model from Google Drive if not present
 def extract_drive_id(url):
@@ -23,21 +23,15 @@ def download_model():
     if not os.path.exists(MODEL_DIR):
         os.makedirs(MODEL_DIR)
 
-    if not os.path.exists(MODEL_PATH):
-        st.info("📥 Downloading model weights...")
+    if not os.path.exists(os.path.join(MODEL_DIR, "tf_model.h5")):
+        st.info("📥 Downloading zipped model...")
         try:
-            response = requests.get(SHARE_URL, allow_redirects=True, headers={"User-Agent": "Mozilla/5.0"})
-            if response.status_code == 200:
-                with open(MODEL_PATH, "wb") as f:
-                    f.write(response.content)
-
-                if os.path.getsize(MODEL_PATH) < 1000000:
-                    raise ValueError("❌ Model file too small. Dropbox may have returned HTML instead of the file.")
-                st.success("✅ Model downloaded successfully.")
-            else:
-                st.error(f"❌ Dropbox link failed with status code: {response.status_code}")
+            r = requests.get(SHARE_URL)
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extractall(MODEL_DIR)
+            st.success("✅ Model unzipped and ready!")
         except Exception as e:
-            st.error(f"❌ Error downloading model: {e}")
+            st.error(f"❌ Failed to unzip model: {e}")
 
 
 
